d2de1f1b3c2aff2377a4caa685d4577aeb618f0a
Brian Bischoff <bbischoff78@gmail.com>
2022-01-07 09:14:15 -0500

    fixed remote unit test errors

---
+++ src/F3/Settings.php
@@ -0,0 +1,15 @@
+<?php
+namespace F3;
+
+class Settings {
+
+	const DB_HOST = '';
+	const DB_NAME = '';
+	const DB_USER = '';
+	const DB_PASS = '';
+	const DB_CHARSET = 'utf8';
+	
+	const GA_TRACKING_CODE = '';
+}
+
+?>
--- test/F3/Repo/MemberRepositoryTest.php
+++ test/F3/Repo/MemberRepositoryTest.php
@@ -247,14 +247,14 @@ class MemberRepositoryTest extends TestCase {
         $this->pdoMock->method('prepare')
                               ->willReturn($statementMock);
         $this->pdoMock->method('lastInsertId')
-                              ->willReturn(1);
+                              ->willReturn('1');
                         
         $statementMock->expects($this->once())
                       ->method('execute');
 
         $memberRepo = new MemberRepository($this->database);
         $id = $memberRepo->save('Splinter');
-        $this->assertEquals(1, $id, 'inserted ID mismatch');
+        $this->assertEquals('1', $id, 'inserted ID mismatch');
     }
 
     public function testDelete() {
